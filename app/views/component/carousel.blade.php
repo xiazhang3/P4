8e615d2d7e981a7c8a925815d532ece1406272a1
Xia Zhang <xianiu3@gmail.com>
2014-07-24 11:53:23 -0400

    index page

---
+++ app/views/component/carousel.blade.php
@@ -0,0 +1,34 @@
+<div id="myCarousel" class="carousel slide">
+
+    <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+  
+    </ol><!-- carousel-indicators -->
+
+    <section class="carousel-inner">
+        <div class="active item"><img src="http://placekitten.com/1200/500 " alt="e">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Recommendation Letter Organizer</h1>
+              <p>Track request for recommendation letters, information of letter recipients, and more</p>
+              <p><a class="btn btn-lg btn-warning" href="#" role="button">Try Now For Free</a></p>
+            </div>
+          </div>
+        </div>
+
+        <div class="item"><img src="http://placekitten.com/1200/600 " alt="">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Job/University Application Organizer</h1>
+              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+              <p><a class="btn btn-lg btn-warning" href="#" role="button">Learn more</a></p>
+            </div>
+          </div>
+
+        </div>
+    </section><!-- carousel-inner -->
+
+    <a href="#myCarousel" class="left carousel-control" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+    <a href="#myCarousel" class="right carousel-control" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+</div><!-- myCarousel -->
---
+++ app/views/component/lefticon.php
@@ -0,0 +1,47 @@
+<div class="lefticon col-lg-6">
+			<br>
+			<div class="col-sm-6 theBest">
+					<div class="media">
+                    	<div class="pull-left">
+                    		<img src="/img/web.png" alt="...">
+                        </div>
+						<div class="media-body">Web-based</div>
+                    </div>
+				
+					<div class="media">
+                    	<div class="pull-left">
+                    		<img src="/img/setting.png" alt="...">
+                        </div>
+						<div class="media-body">Customizable</div>
+                    </div>
+
+					<div class="media">
+                    	<div class="pull-left">
+                    		<img src="/img/lock.png" alt="...">
+                        </div>
+						<div class="media-body">Fully Secure</div>
+                    </div>
+				</div>
+
+				<div class="col-sm-6 theBest">
+					<div class="media">
+                    	<div class="pull-left">
+                    		<img src="/img/phone.png" alt="...">
+                        </div>
+						<div class="media-body">Tech Support</div>
+					</div>
+                    <div class="media">
+                        <div class="pull-left">
+                            <img src="/img/free.png" alt="...">
+                        </div>
+                        <div class="media-body">Free Sign Up</div>
+                    </div>
+                    <div class="media">
+                        <div class="pull-left">
+                            <img src="/img/color.png" alt="...">
+                        </div>
+                        <div class="media-body">Feature Rich</div>
+                    </div>
+                </div>
+
+		</div> <!--lefticon-->
